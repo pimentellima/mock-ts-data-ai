86ff1c7266ed88f96a4807ce2d10c599d29c71de
pimentellima <matheus_dere@hotmail.com>
2024-06-09 15:50:10 -0300

    adicionado resultado padrão para visualização antes de gerar resultado

--- src/app/generator-wrapper.tsx
+++ src/app/generator-wrapper.tsx
@@ -7,7 +7,16 @@ import ResultsList from "./results-list"
 export default function GeneratorWrapper() {
     const [result, setResult] = useState<
         { resultName: string; json: string }[] | null
-    >(null)
+    >([
+        {
+            resultName: "User",
+            json: '[{\n  "id": 1,\n  "name": "John Doe",\n  "age": 30\n},{\n  "id": 2,\n  "name": "Jane Smith",\n  "age": 25\n},{\n  "id": 3,\n  "name": "Alice Johnson",\n  "age": 35\n},{\n  "id": 4,\n  "name": "Bob White",\n  "age": 27\n},{\n  "id": 5,\n  "name": "Eve Brown",\n  "age": 40\n}]',
+        },
+        {
+            resultName: "Friend",
+            json: '[{\n  "userId": 1,\n  "friendId": 2\n},{\n  "userId": 2,\n  "friendId": 3\n},{\n  "userId": 3,\n  "friendId": 4\n},{\n  "userId": 4,\n  "friendId": 5\n},{\n  "userId": 5,\n  "friendId": 1\n}]',
+        },
+    ])
 
     return (
         <div className="grid grid-cols-2 gap-5">
